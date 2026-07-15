49034ca599ffcca664dae6e8b8bdf7441c6717ce
Abdallah <tarqb131@gmail.com>
2026-07-15 17:21:32 +0300

    Integrate API functionality and add HTTP dependency for data fetching

---
+++ lib/data/api.dart
@@ -0,0 +1,19 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+abstract class Api {
+  //GET https://newsapi.org/v2/everything?q=bitcoin&apiKey=d63f1e28b9334ce7afcfe80f366c7230
+
+  Future<Map<String, dynamic>> getData() async {
+    Uri url = Uri.https("newsapi.org", "/v2/everything", {
+      'q': 'bitcoin',
+      'apiKey': 'd63f1e28b9334ce7afcfe80f366c7230',
+    });
+
+    var response = await http.get(url);
+    var responseBody = response.body;
+    Map<String, dynamic> json = jsonDecode(responseBody);
+    return json;
+  }
+}
